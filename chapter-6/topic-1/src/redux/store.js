74058050f894d888c8509feefc08a0644e38b31c
Fahmi Alfareza <fahmialfareza@icloud.com>
2023-05-12 21:16:35 +0700

    chapter 6: 3

--- chapter-6/topic-1/src/redux/store.js
+++ chapter-6/topic-1/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 import rootReducers from "./reducers";
 
 // Create the store
 export default configureStore({
   reducer: rootReducers,
   devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // It not necessary if your feature is not too complex, you can just comment this line if you don't need it
 });
